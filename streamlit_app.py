7b74e48ef7a6e44d913fddb572f2ccf66f8f567d
Chong Wai Yip <cranageist@gmail.com>
2025-09-05 15:58:40 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Prediction of Online Gaming Behaviour")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
