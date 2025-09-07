d3703fc6f97b2043ad8967354ad216af4e5666e3
Chong Wai Yip <cranageist@gmail.com>
2025-09-07 12:32:17 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,32 +19,49 @@ sd_scl = joblib.load('models/scaler.joblib')
 svm = SVC()
 svm = joblib.load('models/base_ovr_73.joblib')
 
+## PAGE
 st.set_page_config(
-    page_title='AI Dashboard',
+    page_title='Model Predictions',
     layout='wide'
 )
-st.title("Prediction of Online Gaming Behaviour")
-st.write("Predict the Engagement Level of players using CatBoost, RandomForest and SVM models.")
+st.title('Prediction of Online Gaming Behaviour')
+st.write('Predict the Engagement Level of players using CatBoost, RandomForest and SVM models.')
+st.write('Upload a .csv file to run multiple predictions at once, or remove the file to run single predictions.')
 
-sessions_per_week = st.number_input("Sessions played per week", min_value=0, max_value=50)
-avg_session_duration_mins = st.number_input("Average session duration (mins)", min_value=0, max_value=600)
-achievements_unlocked = st.number_input("Achievements unlocked", min_value=0, max_value=100)
-player_level = st.number_input("Player level", min_value=0, max_value=100)
+sessions_per_week = st.number_input('Sessions played per week', min_value=0, max_value=50)
+avg_session_duration_mins = st.number_input('Average session duration (mins)', min_value=0, max_value=600)
+achievements_unlocked = st.number_input('Achievements unlocked', min_value=0, max_value=100)
+player_level = st.number_input('Player level', min_value=0, max_value=100)
 
-if st.button("Predict"):
-    X = pd.DataFrame(
-        {
-            'SessionsPerWeek':[sessions_per_week],
-            'AvgSessionDurationMinutes':[avg_session_duration_mins],
-            'AchievementsUnlocked':[achievements_unlocked],
-            'PlayerLevel':[player_level]
-        }
-    )
+st.write('Alternatively, upload a file below.')
+csv = st.file_uploader('Upload .csv', type='csv')
+
+if st.button('Predict'):
+    try:
+        X = pd.read_csv(csv)
+    except:
+        X = pd.DataFrame(
+            {
+                'SessionsPerWeek':[sessions_per_week],
+                'AvgSessionDurationMinutes':[avg_session_duration_mins],
+                'AchievementsUnlocked':[achievements_unlocked],
+                'PlayerLevel':[player_level]
+            }
+        )
+    
     rf_pred = rf.predict(X)
     cb_pred = cb.predict(X)
     svm_pred = svm.predict(sd_scl.transform(X))
 
+    cb_strung = []
+    svm_strung = []
+
+    for i in range(len(cb_pred)):
+        cb_strung.append(cb_labels[int(cb_pred[i])])
+        svm_strung.append(svm_labels[int(svm_pred[i])])
+
+    X['CatBoost'] = cb_strung
+    X['RandomForest'] = rf_pred
+    X['SVM'] = svm_strung
+
     st.write(X)
-    st.write(f'CatBoost prediction: {cb_labels[int(cb_pred)]}')
-    st.write(f'RandomForest prediction: {rf_pred[0]}')
-    st.write(f'Support Vector machine prediction: {svm_labels[int(svm_pred)]}')
